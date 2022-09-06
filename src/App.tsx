0c1d36d980f2d5d7a5ed2319bad007fdcad56279
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-09-06 06:56:17 -0300

    initial config new project

---
+++ src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <div className="text-title-500">Github blog</div>;
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "Nunito, sans-serif",
+      },
+      colors: {
+        blue: {
+          500: "#3294F8",
+        },
+        title: {
+          500: "#E7EDF4",
+        },
+        subtitle: {
+          500: "#C4D4E3",
+        },
+        text: {
+          500: "#AFC2D4",
+          700: "#7B96B2",
+          900: "#3A536B",
+        },
+        border: {
+          500: "#1C2F41",
+        },
+        post: {
+          500: "#112131",
+        },
+        profile: {
+          500: "#0B1B2B",
+        },
+        background: {
+          500: "#071422",
+        },
+        input: {
+          500: "#040F1A",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
