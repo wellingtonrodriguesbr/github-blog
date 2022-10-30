245b4d7b07e011e191b67c2c2c9ef29d8dde68d6
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-30 09:45:18 -0300

    chore: change cover

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import { Container } from "../components/Container";
+import { Posts } from "../components/Posts";
 import { Profile } from "../components/Profile";
+import logoImg from "../assets/Logo.svg";
 
 export function Home() {
   return (
     <>
-      <div className="w-full h-[350px] bg-bg-cover bg-cover bg-no-repeat bg-center" />
+      <div className="w-full h-[350px] bg-bg-cover bg-cover bg-no-repeat bg-center flex items-center justify-center">
+        <img className="mb-16" src={logoImg} alt="" />
+      </div>
 
       <main className="flex flex-col justify-center items-center h-full">
         <Container>
           <div className="-mt-24 w-full h-full flex justify-center">
             <Profile />
           </div>
+          <Posts />
         </Container>
       </main>
     </>
