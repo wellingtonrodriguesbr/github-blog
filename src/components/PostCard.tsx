d3523ba069ff83236697e5fcc5384abad82217ee
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-30 09:45:41 -0300

    feat: create PostCard component

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,18 @@
+export function PostCard() {
+  return (
+    <div className="p-8 rounded-[10px] bg-post-500">
+      <div className="flex justify-between gap-4">
+        <h3 className="text-xl text-title-500 font-bold flex-1">
+          JavaScript data types and data structures
+        </h3>
+        <p className="text-sm text-text-700">Há 1 dia</p>
+      </div>
+
+      <p className="text-base text-text-500 mt-5">
+        Programming languages all have built-in data structures, but these often
+        differ from one language to another. This article attempts to list the
+        built-in data structures available in
+      </p>
+    </div>
+  );
+}
