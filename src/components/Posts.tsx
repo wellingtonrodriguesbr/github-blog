fa6c69f0fc4bafb7fc1fb12cdeade46dca1fb5e9
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-30 09:44:25 -0300

    feat: create Posts component

---
+++ src/components/Posts.tsx
@@ -0,0 +1,24 @@
+import { PostCard } from "./PostCard";
+
+export function Posts() {
+  return (
+    <section className="mt-[72px] mb-16">
+      <div className="flex items-center justify-between mb-3">
+        <strong className="text-lg text-subtitle-500">Publicações</strong>
+        <p className="text-sm text-text-700">6 publicações</p>
+      </div>
+      <input
+        className="w-full py-3 px-4 rounded-md bg-input-500 text-text-900 border border-border-500"
+        type="text"
+        placeholder="Buscar conteúdo"
+      />
+
+      <div className="grid grid-cols-2 gap-8 mt-12">
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </div>
+    </section>
+  );
+}
