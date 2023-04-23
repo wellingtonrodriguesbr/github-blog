e0ff9628ea2d507781411ed667b79c183204ca7f
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-04-23 11:59:37 -0300

    feat: get data of profile, with github api

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -5,13 +5,15 @@ export function Posts() {
     <section className="mt-[72px] mb-16">
       <div className="flex items-center justify-between mb-3">
         <strong className="text-lg text-subtitle-500">Publicações</strong>
-        <p className="text-sm text-text-700">6 publicações</p>
+        <p className="text-sm text-text-700">4 publicações</p>
       </div>
-      <input
-        className="w-full py-3 px-4 rounded-md bg-input-500 text-text-900 border border-border-500"
-        type="text"
-        placeholder="Buscar conteúdo"
-      />
+      <form action="">
+        <input
+          className="w-full py-3 px-4 rounded-md bg-input-500 text-white placeholder:text-text-900 border border-border-500"
+          type="text"
+          placeholder="Buscar conteúdo"
+        />
+      </form>
 
       <div className="grid grid-cols-2 gap-8 mt-12">
         <PostCard />
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,44 +5,60 @@ import {
   faUserGroup,
   faArrowUpRightFromSquare,
 } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+
+interface Profile {
+  html_url: string;
+  avatar_url: string;
+  company: string;
+  name: string;
+  bio: string;
+  followers: number;
+  login: number;
+}
 
 export function Profile() {
+  const [data, setData] = useState<Profile>({} as Profile);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/wellingtonrodriguesbr")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div className="w-full rounded-[10px] bg-profile-500 shadow-sm py-8 px-10">
       <div className="flex gap-8">
-        <div className="min-w-[148px] min-h-[148px] bg-slate-500 rounded-lg"></div>
-        <div className="flex flex-col gap-8">
+        <div className="max-w-[148px] max-h-[148px] bg-slate-500 rounded-lg overflow-hidden">
+          <img className="w-full h-full" src={data.avatar_url} alt="" />
+        </div>
+        <div className="flex flex-col gap-8 w-full">
           <div>
             <div className="flex justify-between items-center">
-              <h1 className="font-bold text-2xl text-title-500">
-                Wellington Rodrigues
-              </h1>
+              <h1 className="font-bold text-2xl text-title-500">{data.name}</h1>
               <a
-                href="#"
+                href={data.html_url}
+                target="_blank"
                 className="uppercase text-blue-500 font-bold text-xs flex gap-2 items-center"
               >
                 github
                 <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
               </a>
             </div>
-            <p className="text-base text-text-500 mt-2">
-              Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-              viverra massa quam dignissim aenean malesuada suscipit. Nunc,
-              volutpat pulvinar vel mass.
-            </p>
+            <p className="text-base text-text-500 mt-2">{data.bio}</p>
           </div>
           <footer className="flex gap-6 items-center">
             <p className="text-base text-subtitle-500 flex gap-2 items-center">
               <FontAwesomeIcon icon={faGithub} size="lg" />
-              wellingtonrodriguesbr
+              {data.login}
             </p>
             <p className="text-base text-subtitle-500 flex gap-2 items-center">
               <FontAwesomeIcon icon={faBuilding} />
-              Gonew.co
+              {data.company}
             </p>
             <p className="text-base text-subtitle-500 flex gap-2 items-center">
               <FontAwesomeIcon icon={faUserGroup} />
-              72 seguidores
+              {data.followers} seguidores
             </p>
           </footer>
         </div>
